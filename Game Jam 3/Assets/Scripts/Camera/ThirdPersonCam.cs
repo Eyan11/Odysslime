3d82f8b8f894f43bb6c81dfe77fb3397ea54c361
Eyan <eyanmartucci11@gmail.com>
2024-03-16 16:14:13 -0700

    Slime Camera, King Movement, King and Slime Possess
    - Added a third person camera for slime
    - Made the king camera top down
    - Finished king movement
    - Made KingPossess script for the king to hover mouse over slime and press "E" to possess them
    - Made a SlimePossess script for a slime to hover mouse over slime or king and press "E" to possess them. Also automatically possesses king when pressing "Z"
    - Made a SwitchCamera method in ThirdPersonCam script to switch camera perspective when possessing different slimes
    - Made Cameras, slime king, ice slime, and explosive slime into a prefab (need to adjust references later, but you can copy from my test scene)
    - Added "Floor" and "Wall" layers so camera will not go through these layers
    - Added "Slime Follower" and "Slime King" tags to tell if player is trying to possess a follower or king
    - Added "Obj" and "Orientation" tags for the obj and orientation objects on the slime players to tell the camera which object they are looking at when changing cameras.

--- Game Jam 3/Assets/Scripts/Camera/ThirdPersonCam.cs
+++ Game Jam 3/Assets/Scripts/Camera/ThirdPersonCam.cs
@@ -1,22 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class ThirdPersonCam : MonoBehaviour
 {
     [Header("References")]
+    //these is for the object you start the game as (slime king)
     [SerializeField] private Transform orientation;
     [SerializeField] private Transform player;
-    [SerializeField] private Transform playerObj;
+    [SerializeField] private Transform obj;
     [SerializeField] private Rigidbody rb;
+
+    [SerializeField] private GameObject thirdPersonCam;
+    [SerializeField] private CinemachineFreeLook thirdPersonFreeLookCam;
+    [SerializeField] private GameObject topDownCam;
 
     [Header("Settings")]
     [SerializeField] private float rotationSpeed;
 
-    private void Start() {
+    private void Awake() {
         //makes mouse invisible and locked in place
         Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
+        //Cursor.visible = false;
     }
 
     private void Update() {
@@ -34,8 +40,51 @@ public class ThirdPersonCam : MonoBehaviour
 
         //smoothly change (Slerp) player orientation to match the input direction
         if(inputDir != Vector3.zero)
-            playerObj.forward = Vector3.Slerp(playerObj.forward, inputDir.normalized, Time.deltaTime * rotationSpeed);
+            obj.forward = Vector3.Slerp(obj.forward, inputDir.normalized, Time.deltaTime * rotationSpeed);
 
+    }
+
+
+    //Called when camera needs to be switched because of possess ability
+    public void SwitchCamera(GameObject slimePlayer) {
+        //disable all camera's
+        thirdPersonCam.SetActive(false);
+        topDownCam.SetActive(false);
+
+        //switch to king camera settings
+        if(slimePlayer.CompareTag("Slime King")) {
+            //enable top down cam
+            topDownCam.SetActive(true);
+        }
+        //switch to slime camera settings
+        else {
+            //enable third person cam
+            thirdPersonCam.SetActive(true);
+
+            //change settings to follow designated slime
+            thirdPersonFreeLookCam.m_LookAt = slimePlayer.transform;
+            thirdPersonFreeLookCam.m_Follow = slimePlayer.transform;
+        }
+
+        //get references for player rotation when moving
+        player = slimePlayer.transform;
+        rb = slimePlayer.GetComponent<Rigidbody>();
+
+        //get obj, should be child(0), but checking both just in case someone moves it
+        if(slimePlayer.gameObject.transform.GetChild(0).CompareTag("Obj"))
+            obj = slimePlayer.gameObject.transform.GetChild(0);
+        else if(slimePlayer.gameObject.transform.GetChild(1).CompareTag("Obj"))
+            obj = slimePlayer.gameObject.transform.GetChild(1);
+        else
+            Debug.Log("Can't get obj reference. Make sure Slime obj is first child of slime player!");
+        
+        //get orientation, should be child(1), but checking both just in case someone moves it
+        if(slimePlayer.gameObject.transform.GetChild(1).CompareTag("Orientation"))
+            orientation = slimePlayer.gameObject.transform.GetChild(1);
+        else if(slimePlayer.gameObject.transform.GetChild(0).CompareTag("Orientation"))
+            orientation = slimePlayer.gameObject.transform.GetChild(0);
+        else
+            Debug.Log("Can't get orientation reference. Make sure Slime obj is second child of slime player!");
     }
 
 
---
+++ Game Jam 3/Assets/Scripts/Slime King/KingMovement.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KingMovement : MonoBehaviour
+{
+    [Header("Movement")]
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private Transform orientation;
+    [SerializeField] private Transform kingObj;
+    
+    private float xInput;
+    private float zInput;
+    private float yInput;
+    private Vector3 moveDir;
+    private Rigidbody rb;
+
+    private void Awake() {
+        //grab rigidbody reference and make sure body doesn't fall over
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+    }
+
+    private void Update() {
+        GetInput();
+
+        //set velocity
+        rb.velocity = moveDir * moveSpeed;
+    }
+
+    private void GetInput() {
+        //get X axis input
+        xInput = Input.GetAxisRaw("Horizontal");
+        //get z axis input
+        zInput = Input.GetAxisRaw("Vertical");
+
+        //get y axis input (space is up, shift is down)
+        if(Input.GetKey(KeyCode.Space))
+            yInput = 1;
+        else if(Input.GetKey(KeyCode.LeftShift))
+            yInput = -1;
+        else
+            yInput = 0;
+
+
+        //find direction of input based on player orientation (relative to camera)
+        moveDir = (orientation.forward * zInput) + (kingObj.up * yInput) + (orientation.right * xInput);
+        //y axis moveDir is based on obj because I want king to move up/down relative to the obj not the camera direction
+    }
+
+}
