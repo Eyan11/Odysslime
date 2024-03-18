bcd9cce9f867e7a2a1aea8dc3b8b8957276ed7aa
Laytonb12 <147658930+Laytonb12@users.noreply.github.com>
2024-03-18 12:32:24 -0700

    UI Update

---
+++ Game Jam 3/Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public bool isStart;
+    public bool isQuit;
+    public string sceneName;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnMouseUp()
+    {
+        if (isStart)
+        {
+            changeScene();
+        }
+        if (isQuit)
+        {
+            Application.Quit();
+        }
+    }
+
+    public void changeScene()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
