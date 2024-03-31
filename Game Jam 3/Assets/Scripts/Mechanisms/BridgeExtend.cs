f4ded81522f2d58af07574a91e4d1a2480a56201
Eyan <eyanmartucci11@gmail.com>
2024-03-30 19:29:09 -0700

    Progress on Bridge
    - Made a little bit of progress on extending bridge
    - Made the pressure plate into it's own prefab again

--- Game Jam 3/Assets/Scripts/Mechanisms/BridgeExtend.cs
+++ Game Jam 3/Assets/Scripts/Mechanisms/BridgeExtend.cs
@@ -2,24 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GateOpen : MonoBehaviour
+public class BridgeExtend : MonoBehaviour
 {
-    [SerializeField] private GameObject gate;
-    [SerializeField] private float slideSpeed;
+    [SerializeField] private GameObject movingBridge;
+    [SerializeField] private Transform bridgeStart;
+    [SerializeField] private Transform bridgeEnd;
+    [SerializeField] private float extendSpeed;
     private float lerpPercent = 0f;
     private Vector3 startPos;
     private Vector3 endPos;
     private bool isActivated = false;
 
     private void Awake() {
+        //get end position based on average position of start/end platforms (in-between them)
+        endPos = (bridgeStart.position + bridgeEnd.position)/2;
         //get initial poistion of gate
-        startPos = gate.transform.position;
-        //get end position based on average position of static walls (in-between them)
-        endPos = (gameObject.transform.GetChild(1).position + gameObject.transform.GetChild(2).position)/2;
+        startPos = bridgeStart.position - endPos;
+
+        //movingBridge.transform.position = startPos;
+        float bridgeLength = Vector3.Distance(startPos, endPos);
+        movingBridge.transform.localScale = new Vector3(bridgeStart.localScale.x, bridgeStart.localScale.y/2, bridgeLength);
+
     }
 
     private void Update() {
-
+        /*
         //if active and not done opening
         if(isActivated && lerpPercent < 1) {
             //open gate
@@ -32,6 +39,7 @@ public class GateOpen : MonoBehaviour
             gate.transform.position = Vector3.Lerp(gate.transform.position, startPos, lerpPercent);
             lerpPercent -= slideSpeed * Time.deltaTime;
         }
+        */
 
     }
 
