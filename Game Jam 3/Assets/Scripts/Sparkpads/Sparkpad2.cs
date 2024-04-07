45ae78ccc1c4cadcda5606ee2c31c86d64f560a9
Jayce <jmcochell04@gmail.com>
2024-04-07 02:27:15 -0700

    Engineer Island and some Coven Island

---
+++ Game Jam 3/Assets/Scripts/Sparkpads/Sparkpad2.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//when a Spark Slime triggers this Sparkpad, set  
+//GameObject "Falling Platform" > Constraints > Freeze Rotation > X 
+//to "false"
+
+public class Sparkpad2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
