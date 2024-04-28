f41dfa2a0082ecd4fd6d58a1b04637c9576a31ca
Eyan <eyanmartucci11@gmail.com>
2024-04-28 16:57:39 -0700

    Finished Slime Trophy Room Spawner
    - Rearranged the banners and trophies in Trophy Room to make room for slimelings
    - Created TrophySlimeSpawner script that spawns the collected amount of slimelings at each island in front of their banners
    - Added in Exit Wall to Trophy Room (although it does not work rn)
    - Applied NavMesh Surface to Trophy Room so all slimes can move on surface, but I set their speeds to 0 so they only look at you

--- Game Jam 3/Assets/Scripts/Managers and UI/TrophySlimeSpawner.cs
+++ Game Jam 3/Assets/Scripts/Managers and UI/TrophySlimeSpawner.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class TrophySlimeSpawner : MonoBehaviour
 {
@@ -14,6 +15,8 @@ public class TrophySlimeSpawner : MonoBehaviour
     private SaveManager saveScript;
     private int slimesCollected;
     private int numSlimePositions;
+    private Vector3 spawnPos;
+    private GameObject slimeInstance;
 
     private void Awake() {
         saveScript = GameObject.FindWithTag("Save Manager").GetComponent<SaveManager>();
@@ -23,9 +26,11 @@ public class TrophySlimeSpawner : MonoBehaviour
         //get the transform component of all children
         spawnPositions = GetComponentsInChildren<Transform>(true);
         numSlimePositions = spawnPositions.Length;
+        Debug.Log("numSlimePositions" + numSlimePositions);
 
         //get slimes collected and make sure there are enough spawn positions
         slimesCollected = saveScript.GetIslandSlimesCollected(islandChar);
+
         if(slimesCollected > numSlimePositions) {
             Debug.LogError("Need to place more spawner positions for island char: " + islandChar);
             return;
@@ -34,8 +39,14 @@ public class TrophySlimeSpawner : MonoBehaviour
         //for every slimeling collected in island
         for(int i = 0; i < slimesCollected; i++) {
 
-            //spawn a random slime in slimePrefabList at the position of spawnPositions array
-            Instantiate(slimePrefabList[Random.Range(0, slimePrefabSize-1)], spawnPositions[i].position, transform.rotation);
+            //set SpawnPos to spawnPosition plus a random XZ plane offset between -1 and 1
+            spawnPos = spawnPositions[i].position + new Vector3(Random.Range(-1f, 1f), 0f, Random.Range(-1f, 1f));
+
+            //spawn a random slime in slimePrefabList at spawnPos
+            slimeInstance = Instantiate(slimePrefabList[Random.Range(0, slimePrefabSize-1)], spawnPos, transform.rotation);
+
+            //Stop all slimeling movement
+            slimeInstance.GetComponent<NavMeshAgent>().speed = 0f;
         }
 
     }
